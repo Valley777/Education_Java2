0a6d25f8503822c32fda927b57242db589ba8451
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-16 19:07:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+	package methods;
+     public class Main {
+	   static void printReceiptHeader() {
+        System.out.println("CASH RECEIPT");
+        }
+	
+	public static void main(String[]args) {
+		printReceiptHeader();
+	}
+
+
+
+
+}
+
+	
