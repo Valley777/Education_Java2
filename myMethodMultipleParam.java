63c095b0cc9553e6552b974e4a890d65be04e1c8
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-16 19:29:06 +0300

    Add files via upload

---
+++ myMethodMultipleParam.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class myMethodMultipleParam {
+    static void myMethod(String nameN, int age) {
+    	System.out.println(nameN+" is"+age);
+    }
+	public static void main(String[] args) {
+		myMethod("BJ", 5);
+		myMethod("Eli", 7);
+		myMethod("Gabata",8);
+
+	}
+
+}
