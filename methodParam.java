cc0893d21fafb32fcf752fea6619f29ef00018f4
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-16 19:21:40 +0300

    Add files via upload

---
+++ methodParam.java
@@ -0,0 +1,18 @@
+package methods;
+public class methodParam{
+	static void myMethod(String fName) {
+		System.out.println(fName+"Astra");
+	}
+
+		public static void main(String[] args) {
+			myMethod("LeeAnn");
+			myMethod("Jenny");
+			myMethod("Phyl");
+			myMethod("Abby");
+			myMethod("Gabche");
+
+		}
+
+	}
+
+
