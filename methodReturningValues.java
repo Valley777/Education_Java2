73d30bf4a89b0b27d27d006698759b7595ed0f92
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-16 19:33:24 +0300

    Add files via upload

---
+++ methodReturningValues.java
@@ -0,0 +1,13 @@
+package methods;
+
+public class methodReturningValues {
+static int myMethod(int x) {
+	return 5+x;
+	
+}
+	public static void main(String[] args) {
+		System.out.println(myMethod(3));
+//8
+	}
+
+}
