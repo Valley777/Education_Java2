24ffbe99c0b5cfeca6bc0960361ad0cb8d004061
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-16 19:40:11 +0300

    Add files via upload

---
+++ staticIntMethodreturningValues.java
@@ -0,0 +1,11 @@
+package methods;
+
+public class staticIntMethodreturningValues {
+       static int myMethod(int x, int y) {
+    	   return x + y;
+       }
+	public static void main(String[] args) {
+       System.out.println(myMethod(5, 3));
+       //Output 8
+	}
+}
