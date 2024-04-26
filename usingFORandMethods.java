83d3bccbebe7039b096586c99d829ca3da4fd18a
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-26 11:42:13 +0300

    Add files via upload

---
+++ usingFORandMethods.java
@@ -0,0 +1,12 @@
+public class usingFORandMethods {
+    static void printNumbers(int start, int end){
+        for (int i = start ;i<=end; i++){
+            System.out.printf("%d",i);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumbers(1,10);
+    }
+}
