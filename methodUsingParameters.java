747dbb3f7552377887a27541aa1210af18983e59
Valentina Nikolaeva <61709121+Valley777@users.noreply.github.com>
2024-04-26 10:23:28 +0300

    Add files via upload

---
+++ methodUsingParameters.java
@@ -0,0 +1,10 @@
+public class methodUsingParameters {
+
+    static void myMethod(String firstName,String lastName){  //using two Strings
+        System.out.println("Dear " +firstName+" "+lastName);
+    }
+    public static void main(String[] args) {
+        myMethod("Valley", "Val");
+
+    }
+}
